af8cdb8b5ced3fae22d3a9eb99a79fde15733671
Shain Wai Yan <shainwaiyan2002@gmail.com>
2025-04-09 14:03:40 +0630

    Fix CORS config to allow frontend domain

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,27 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: ["https://personal-web-deploy.vercel.app"], // Your frontend
+      headers: [
+        "Content-Type",
+        "Authorization",
+        "Origin",
+        "Accept",
+        "cache-control", // Important!
+      ],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      credentials: true,
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
